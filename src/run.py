33765305a85781467fe545c0be7ff88e19f590ff
matthewdhull <matthewdhull@gmail.com>
2023-02-15 09:22:04 -0600

    include cfg param to pass tgt scene name

--- src/run.py
+++ src/run.py
@@ -67,7 +67,7 @@ def run(cfg: DictConfig) -> None:
         os.chdir(original_cwd)
         
         # make predictions using the same camera angles utilized for producing perturbation
-        render_predict = f"make TARGET={target} RESULTS_DIR={cfg.sysconfig.log_dir} TEX_NUM={passes[i]} SENSOR_POS_FN={cfg.scenario.sensor_positions.function} render_predict"
+        render_predict = f"make TARGET={target} TARGET_SCENE={cfg.attack.scene.name} RESULTS_DIR={cfg.sysconfig.log_dir} TEX_NUM={passes[i]} SENSOR_POS_FN={cfg.scenario.sensor_positions.function} render_predict"
         subprocess.run(render_predict, shell=True, check=True)
 
         next_tex = os.path.join(tex_dir, f"tex_{passes[i]}.png")
