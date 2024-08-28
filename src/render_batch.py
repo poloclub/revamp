4db0e76d319aa28571afefb6f1e8497059546783
Matthew Hull <matthewdhull@gmail.com>
2024-08-28 13:10:19 -0700

    use batch sensors for batch rendering

--- src/render_batch.py
+++ src/render_batch.py
@@ -10,9 +10,9 @@ import os
 import argparse
 import time
 import ast
-import tqdm
+from tqdm import tqdm
 
-from dt2 import generate_cam_positions_for_lats 
+from dt2 import generate_cam_positions_for_lats, generate_batch_sensor
 
 if __name__  == "__main__":
     parser = argparse.ArgumentParser( \
@@ -34,17 +34,60 @@ if __name__  == "__main__":
     mi.set_variant("cuda_ad_rgb")
     scene = mi.load_file(args.scene)
     
-    camera_positions = generate_cam_positions_for_lats(sensor_z_lats \
-                                                        ,args.sensor_radius \
-                                                        , args.sensor_count)
+    camera_positions = generate_cam_positions_for_lats(lats=sensor_z_lats, r=args.sensor_radius, size=args.sensor_count, world_transformed=False)
+    
+    # idea is to "batch the batch" because > 30 sensors causes out of memory error.
+    # first idea, is if batch is >30, then divide number of sensors by 30 for each batch
+    # if the last batch is < 30, then create a batch of < 30 sensors
+    # e.g., if batch size is 100, then 4 batches of 30,30,30,10 sensors
+    # camera_positions contains the number of camera positions that we need to generate for.
+    # first, divide them up into batches of 30  (or less)
+    
+    batched_camera_positions = []
+    if len(camera_positions) > 30:
+        batch_size = 30
+        num_batches = len(camera_positions) // 30
+        last_batch_size = len(camera_positions) % 30
+        if last_batch_size > 0:
+            num_batches += 1
+        print(f'num_batches={num_batches}')
+        print(f'last_batch_size={last_batch_size}')
+        for i in range(0, num_batches):
+            if i == num_batches - 1:
+                batched_camera_positions.append(camera_positions[i*batch_size:])
+            else:
+                batched_camera_positions.append(camera_positions[i*batch_size:(i+1)*batch_size])
+     
+     # for every batch in batched_camera_positions, generate a batch sensor
+    batch_sensors = []
+    num_batches = len(batched_camera_positions)
+    for i, b in enumerate(batched_camera_positions):
+        print(f'generating batch sensor for batch {i+1} of {num_batches}')
+        batch_sensor_dict = generate_batch_sensor(b, args.sensor_radius, args.sensor_count)
+        batch_sensor = mi.load_dict(batch_sensor_dict)
+        batch_sensors.append(batch_sensor)
+        
     params = mi.traverse(scene)
     spp = args.spp
     cam_key = args.cam_key
     print(f'rendering {len(camera_positions)} imgs...')
-    for i in tqdm(range(0, len(camera_positions))):
-        params[cam_key].matrix = camera_positions[i].matrix
-        params.update()
-        img =  mi.render(scene, params=params, spp=spp, sensor=0, seed=i+1)
-        rendered_img_path = os.path.join(args.outdir,f"render_{i}.png")
-        mi.util.write_bitmap(rendered_img_path, data=img, write_async=False)
-    print('done')
+    for i in tqdm(range(0, len(batch_sensors)), desc='Rendering Batched Images'):
+        # params[cam_key].matrix = camera_positions[i].matrix
+        # params.update()
+        
+        H = 128 #FIXME: hardcoded
+        batch_sensor = batch_sensors[i]
+        num_sensors = batch_sensor.m_film.size()[0]//H
+        img =  mi.render(scene, params=params, spp=spp, sensor=batch_sensor, seed=i+1)
+        
+        # rendered_img_path = os.path.join(args.outdir,f"render_{i}.png")
+        # mi.util.write_bitmap(rendered_img_path, data=img, write_async=False)
+        for j in range(num_sensors):
+            start = j * H
+            end = (j+1) * H
+            split_img = img[:,start:end,:]
+            img_counter = i * batch_size + j
+            rendered_img_path = os.path.join(args.outdir,f"render_{img_counter}.png")
+            mi.util.write_bitmap(rendered_img_path, data=split_img, write_async=False)   
+    
+    print('Finished rendering')
